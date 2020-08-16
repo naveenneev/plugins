98b9f1ee937b423719a1d5a44665ab090be41a74
bytestreme <sapraliyevshynggys@gmail.com>
2020-08-16 17:27:26 +0600

    GoogleMap changes for GroundOverlay

--- packages/google_maps_flutter/google_maps_flutter/android/src/main/java/io/flutter/plugins/googlemaps/GoogleMapBuilder.java
+++ packages/google_maps_flutter/google_maps_flutter/android/src/main/java/io/flutter/plugins/googlemaps/GoogleMapBuilder.java
@@ -27,6 +27,7 @@ class GoogleMapBuilder implements GoogleMapOptionsSink {
   private Object initialPolygons;
   private Object initialPolylines;
   private Object initialCircles;
+  private Object initialGroundOverlays;
   private Rect padding = new Rect(0, 0, 0, 0);
 
   GoogleMapController build(
@@ -60,6 +61,7 @@ class GoogleMapBuilder implements GoogleMapOptionsSink {
     controller.setInitialPolygons(initialPolygons);
     controller.setInitialPolylines(initialPolylines);
     controller.setInitialCircles(initialCircles);
+    controller.setInitialGroundOverlays(initialGroundOverlays);
     controller.setPadding(padding.top, padding.left, padding.bottom, padding.right);
     return controller;
   }
@@ -181,5 +183,10 @@ class GoogleMapBuilder implements GoogleMapOptionsSink {
   @Override
   public void setInitialCircles(Object initialCircles) {
     this.initialCircles = initialCircles;
+  }
+
+  @Override
+  public void setInitialGroundOverlays(Object groundOverlays) {
+    this.initialGroundOverlays = groundOverlays;
   }
 }
